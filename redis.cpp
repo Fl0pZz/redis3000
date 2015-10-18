2faf69205c9aeb2400dec3e2df562d1e78b8885b
fahreevr <fahreevrafael@yandex.ru>
2015-10-18 11:29:22 +0300

    resolve #3 без bulk string типа

--- redis.cpp
+++ redis.cpp
@@ -5,7 +5,32 @@ void WriteRedisValue(Writer* w, const RedisValue& value) {
         w->write_char(':');
         w->write_int(boost::get<int64_t>(value));
         w->write_crlf();
+
+    } else if (value.which() == REDIS_STRING) {
+        w->write_char('+');
+        w->write_string(boost::get<std::string>(value));
+        w->write_crlf();
+
+    } else if (value.which() == REDIS_ERROR) {
+        w->write_char('-');
+        w->write_string(boost::get<RedisError>(value).msg);
+        w->write_crlf();
+
+    } else if (value.which() == REDIS_NULL) {
+        w->write_char('$');
+        w->write_int(static_cast<int64_t>(-1)); //TODO: удостовериться что это верно!
+        w->write_crlf();
+
+    } else if (value.which() == REDIS_ARRAY) {
+        w->write_char('*');
+        w->write_int(boost::get<std::vector<RedisValue>>(value).size());
+        w->write_crlf();
+        for (auto elem : boost::get<std::vector<RedisValue>>(value)) {
+            WriteRedisValue(w, elem);
+        }
+
     } else {
+        //TODO: разобраться с типо bulk string из RESP
         throw std::runtime_error("unsupported type");
     }
 }
